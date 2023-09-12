3bd5732f91013af286ca2315e051239d6a7daf0f
lucasgurgeln <lucasgurgel09@hotmail.com>
2023-09-12 11:28:53 -0300

    teste boa prática

--- firmware/src/asf.h
+++ firmware/src/asf.h
@@ -93,11 +93,23 @@
 // From module: SPI - Serial Peripheral Interface
 #include <spi.h>
 
+// From module: Standard serial I/O (stdio) - SAM implementation
+#include <stdio_serial.h>
+
 // From module: System Clock Control - SAME70 implementation
 #include <sysclk.h>
 
 // From module: TC - Timer Counter
 #include <tc.h>
+
+// From module: UART - Univ. Async Rec/Trans
+#include <uart.h>
+
+// From module: USART - Serial interface - SAM implementation for devices with both UART and USART
+#include <serial.h>
+
+// From module: USART - Univ. Syn Async Rec/Trans
+#include <usart.h>
 
 // From module: pio_handler support enabled
 #include <pio_handler.h>
